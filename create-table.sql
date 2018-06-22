80ce5792240438a0f7361b5e89e7f96a9e740953
Blackdiz <blackdiz@gmail.com>
2018-06-22 22:11:04 +0800

    add creator fk account.user_name in project and issue table

--- create-table.sql
+++ create-table.sql
@@ -2,9 +2,22 @@ drop table if exists issue;
 drop table if exists project;
 drop table if exists account;
 
+create table account(
+user_name varchar(20) not null,
+password varchar(255) not null,
+first_name varchar(20) not null,
+last_name varchar(20) not null,
+email varchar(50) not null,
+last_login_time timestamp,
+create_time timestamp not null,
+last_update_time timestamp not null,
+primary key (user_name)
+);
+
 create table project(
 id serial,
 project_name varchar(50) not null,
+creator varchar(20) references account(user_name),
 create_time timestamp not null,
 last_update_time timestamp not null,
 primary key (id)
@@ -14,7 +27,7 @@ create table issue(
 id serial,
 project_id integer references project(id),
 title varchar(100) not null,
-description varchar(1000),
+description text,
 status varchar(20) not null,
 priority varchar(20),
 assigned_user varchar(100),
@@ -22,19 +35,8 @@ estimate_work_hour smallint,
 estimate_start_date date,
 estimate_end_date date,
 finished_percent smallint,
+creator varchar(20) references account(user_name),
 create_time timestamp not null,
 last_update_time timestamp not null,
 primary key (id)
-);
-
-create table account(
-user_account varchar(20) not null,
-password varchar(255) not null,
-first_name varchar(20) not null,
-last_name varchar(20) not null,
-email varchar(50) not null,
-last_login_time timestamp,
-create_time timestamp not null,
-last_update_time timestamp not null,
-primary key (user_account)
 );
