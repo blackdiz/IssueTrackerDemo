89d60cc42b71ac02e291b26e4b818a88fb59687b
Blackdiz <blackdiz@gmail.com>
2018-06-23 22:07:24 +0800

    1. add tag and insert basic data 2. adduser_project table

--- create-table.sql
+++ create-table.sql
@@ -1,8 +1,21 @@
 drop table if exists issue;
+drop table if exists tag;
+drop table if exists users_project;
 drop table if exists project;
-drop table if exists account;
+drop table if exists users;
 
-create table account(
+--追蹤標籤
+create table Tag(
+name varchar(20) not null,
+primary key (name)
+);
+
+insert into Tag(name) values('Bug');
+insert into Tag(name) values('Feature');
+insert into Tag(name) values('Support');
+
+--帳號
+create table users(
 user_name varchar(20) not null,
 password varchar(255) not null,
 first_name varchar(20) not null,
@@ -14,29 +27,40 @@ last_update_time timestamp not null,
 primary key (user_name)
 );
 
+--專案
 create table project(
-id serial,
+id varchar(100) not null,
 project_name varchar(50) not null,
-creator varchar(20) references account(user_name),
+is_public boolean default true,
+creator varchar(20) references users(user_name),
 create_time timestamp not null,
 last_update_time timestamp not null,
 primary key (id)
 );
 
+--需求
 create table issue(
 id serial,
-project_id integer references project(id),
+project_id varchar(100) references project(id),
 title varchar(100) not null,
 description text,
 status varchar(20) not null,
-priority varchar(20),
+priority varchar(20) not null,
+tag varchar(20) not null references tag(name),
 assigned_user varchar(100),
 estimate_work_hour smallint,
 estimate_start_date date,
 estimate_end_date date,
 finished_percent smallint,
-creator varchar(20) references account(user_name),
+creator varchar(20) references users(user_name),
 create_time timestamp not null,
 last_update_time timestamp not null,
 primary key (id)
+);
+
+--帳號_專案_Mapping
+create table users_project(
+user_name varchar(20) not null references users(user_name),
+project_id varchar(100) not null references project(id),
+primary key(user_name, project_id)
 );
