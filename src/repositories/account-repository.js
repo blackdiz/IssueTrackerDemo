8843966936034d6485d45dc61c0b97995ebcb011
blackdiz <blackdiz@gmail.com>
2018-06-27 20:41:08 +0800

    remove debug()

--- src/repositories/account-repository.js
+++ src/repositories/account-repository.js
@@ -4,8 +4,5 @@ const Account = require('../model/account');
 
 module.exports = {
   save: (account, tx) => Account.query(tx).insert(account),
-  get: (name, tx) =>
-    Account.query(tx)
-      .where('name', name)
-      .debug()
+  get: (name, tx) => Account.query(tx).where('name', name)
 };
