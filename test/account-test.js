17fbb28e07cb8330528d0412b07445eb7975f91a
blackdiz <blackdiz@gmail.com>
2018-06-27 21:28:00 +0800

    1. add test bcrypt hash account.password 2. change file name of test-account.js to account-test.js

---
+++ test/account-test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const { assert } = require('chai');
+const accountRepository = require('../src/repositories/account-repository');
+const { transaction } = require('objection');
+const conn = require('../config/db');
+const bcrypt = require('bcrypt');
+
+describe('account-repository', () => {
+  describe('#save(account)', () => {
+    it('test save account to database and get back', async () => {
+      const tx = await transaction.start(conn);
+      const newAccount = {
+        name: 'name',
+        first_name: 'firstname',
+        last_name: 'lastname',
+        email: '123',
+        create_time: new Date(),
+        last_update_time: new Date()
+      };
+      newAccount.password = await bcrypt.hash('123', 10);
+
+      await accountRepository.save(newAccount, tx);
+      const account = await accountRepository.get('name', tx);
+      assert.strictEqual(1, account.length);
+      assert.strictEqual('name', account[0].name);
+      console.log(account[0]);
+      await tx.rollback();
+    });
+  });
+});
