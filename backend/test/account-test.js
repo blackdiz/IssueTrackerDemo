60b05df0fda6b4484085fac6e65dc01ea3ab2d74
blackdiz <blackdiz@gmail.com>
2018-07-09 15:13:32 +0800

    修正account-test使其可以pass

--- backend/test/account-test.js
+++ backend/test/account-test.js
@@ -2,30 +2,23 @@
 
 const { assert } = require('chai');
 const accountRepository = require('../src/repositories/account-repository');
-const { transaction } = require('objection');
-const conn = require('../src/config/db');
+const transaction = require('../src/utils/transaction');
 const bcrypt = require('bcrypt');
 
 describe('account-repository', () => {
   describe('#save(account)', () => {
     it('test save account to database and get back', async () => {
-      const tx = await transaction.start(conn);
+      const tx = await transaction();
       const newAccount = {
-        name: 'name',
-        firstName: 'firstname',
-        lastName: 'lastname',
-        email: '123',
-        createTime: new Date(),
-        lastUpdateTime: new Date(),
+        name: 'test_name',
         active: true
       };
-      newAccount.password = await bcrypt.hash('123', 10);
+      newAccount.password = await bcrypt.hash('test_password', 10);
 
       await accountRepository.save(newAccount, tx);
-      const account = await accountRepository.get('name', tx);
-      assert.strictEqual(1, account.length);
-      assert.strictEqual('name', account[0].name);
-      console.log(account[0]);
+      const account = await accountRepository.findByName('test_name', tx);
+      console.log(account);
+      assert.strictEqual('test_name', account.name);
       await tx.rollback();
     });
   });
