e6f4c951fd937912a0d076b5d6023dc912673157
blackdiz <blackdiz@gmail.com>
2018-07-11 22:00:05 +0800

    rename function name

--- backend/src/repositories/issue-repository.js
+++ backend/src/repositories/issue-repository.js
@@ -6,13 +6,12 @@ module.exports = {
   addIssueToProject: (project, issue, tx) => project.$relatedQuery('issues', tx).insert(issue),
   findAllByProject: (project, tx) => project.$relatedQuery('issues', tx).orderBy('id', 'asc'),
   findByProjectAndId: (project, id, tx) => project.$relatedQuery('issues', tx).findOne({ id }),
-  updateIssue: (projectId, id, issue, tx) =>
+  update: (projectId, id, issue, tx) =>
     Issue.query(tx)
       .update(issue)
       .where({ project_id: projectId, id }),
-  deleteIssue: (projectId, id, tx) =>
+  delete: (projectId, id, tx) =>
     Issue.query(tx)
       .delete()
       .where({ project_id: projectId, id })
-      .debug()
 };
