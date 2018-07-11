9f4ba8d621f852069e960e6e915e5325df3e4cb7
blackdiz <blackdiz@gmail.com>
2018-07-11 17:18:10 +0800

    問題表單拆分到IssueForm

--- frontend/src/public/issue/issue-editor.js
+++ frontend/src/public/issue/issue-editor.js
@@ -1,68 +1,7 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { Row, Col, Form, FormGroup, Input, Button, Label } from 'reactstrap';
-
-async function fetchTags() {
-  const res = await fetch(API_URL + '/api/issue-option/tag', {
-    method: 'GET',
-    credentials: 'include'
-  });
-  if (res.status === 200) {
-    const tags = await res.json();
-    return tags.map((tag) => {
-      return (
-        <option key={tag.id} value={tag.id}>
-          {tag.name}
-        </option>
-      );
-    });
-  }
-}
-
-async function fetchStatus() {
-  const res = await fetch(API_URL + '/api/issue-option/status', {
-    method: 'GET',
-    credentials: 'include'
-  });
-  if (res.status === 200) {
-    const status = await res.json();
-    return status.map((status) => {
-      return (
-        <option key={status.id} value={status.id}>
-          {status.name}
-        </option>
-      );
-    });
-  }
-}
-
-async function fetchPriorities() {
-  const res = await fetch(API_URL + '/api/issue-option/priority', {
-    method: 'GET',
-    credentials: 'include'
-  });
-  if (res.status === 200) {
-    const priorities = await res.json();
-    return priorities.map((priority) => {
-      return (
-        <option key={priority.id} value={priority.id}>
-          {priority.name}
-        </option>
-      );
-    });
-  }
-}
-
-function getFinishedPercent() {
-  return Array.from(Array(11).keys(), (i) => i * 10).map((num) => {
-    return (
-      <option key={num} value={num}>
-        {num}%
-      </option>
-    );
-  });
-}
+import IssueForm from './issue-form';
 
 class IssueEditor extends Component {
   constructor(props) {
@@ -82,28 +21,12 @@ class IssueEditor extends Component {
         assignedAccount: '',
         estimateWorkHour: 0,
         finishedPercent: 0
-      },
-      tags: [],
-      status: [],
-      priorities: [],
-      finishedPercent: getFinishedPercent()
+      }
     };
   }
 
   componentDidMount() {
     (async () => {
-      const tags = await fetchTags();
-      const status = await fetchStatus();
-      const priorities = await fetchPriorities();
-      if (Array.isArray(tags)) {
-        this.setState({ tags: tags });
-      }
-      if (Array.isArray(status)) {
-        this.setState({ status: status });
-      }
-      if (Array.isArray(priorities)) {
-        this.setState({ priorities: priorities });
-      }
       const res = await fetch(API_URL + `/api${this.props.match.url}`, {
         method: 'GET',
         credentials: 'include'
@@ -146,147 +69,13 @@ class IssueEditor extends Component {
   render() {
     return (
       <div className="mt-3">
-        <Row>
-          <Col sm={{ size: 1, offset: 1 }}>
-            <h2>建立問題</h2>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={{ size: 10, offset: 1 }} className="bg-light">
-            <Form className="mt-3 mb-3" onSubmit={this.handleSubmit}>
-              <FormGroup row>
-                <Col sm="1">
-                  <Label for="tagId">追蹤標籤</Label>
-                </Col>
-                <Col sm="11">
-                  <Input
-                    type="select"
-                    name="tagId"
-                    id="tagId"
-                    required
-                    value={this.state.issue.tagId}
-                    onChange={this.handleInputChange}
-                  >
-                    {this.state.tags}
-                  </Input>
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Col sm="1">
-                  <Label for="title">標題</Label>
-                </Col>
-                <Col sm="11">
-                  <Input
-                    type="text"
-                    name="title"
-                    id="title"
-                    maxLength="100"
-                    required
-                    value={this.state.issue.title}
-                    onChange={this.handleInputChange}
-                  />
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Col sm="1">
-                  <Label for="description">簡述</Label>
-                </Col>
-                <Col sm="11">
-                  <Input
-                    type="textarea"
-                    name="description"
-                    id="description"
-                    value={this.state.issue.description}
-                    onChange={this.handleInputChange}
-                  />
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Col sm="1">
-                  <Label for="statusId">狀態</Label>
-                </Col>
-                <Col sm="5">
-                  <Input
-                    type="select"
-                    name="statusId"
-                    id="statusId"
-                    value={this.state.issue.statusId}
-                    onChange={this.handleInputChange}
-                  >
-                    {this.state.status}
-                  </Input>
-                </Col>
-                <Col sm="1">
-                  <Label for="startDate">開始日期</Label>
-                </Col>
-                <Col sm="5">
-                  <Input type="input" name="startDate" id="startDate" />
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Col sm="1">
-                  <Label for="statusId">優先權</Label>
-                </Col>
-                <Col sm="5">
-                  <Input
-                    type="select"
-                    name="priorityId"
-                    id="priorityId"
-                    value={this.state.issue.priorityId}
-                    onChange={this.handleInputChange}
-                  >
-                    {this.state.priorities}
-                  </Input>
-                </Col>
-                <Col sm="1">
-                  <Label for="endDate">結束日期</Label>
-                </Col>
-                <Col sm="5">
-                  <Input type="input" name="endDate" id="endDate" />
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Col sm="1">
-                  <Label for="assignedAccount">分派給</Label>
-                </Col>
-                <Col sm="5">
-                  <Input type="input" name="assignedAccount" id="assignedAccount" />
-                </Col>
-                <Col sm="1">
-                  <Label for="estimateWorkHour">預估工時</Label>
-                </Col>
-                <Col sm="5">
-                  <Input
-                    type="input"
-                    name="estimateWorkHour"
-                    id="estimateWorkHour"
-                    value={this.state.issue.estimateWorkHour}
-                    onChange={this.handleInputChange}
-                  />
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Col sm="1">
-                  <Label for="finishedPercent">完成百分比</Label>
-                </Col>
-                <Col sm="5">
-                  <Input
-                    type="select"
-                    name="finishedPercent"
-                    id="finishedPercent"
-                    value={this.state.issue.finishedPercent}
-                    onChange={this.handleInputChange}
-                  >
-                    {this.state.finishedPercent}
-                  </Input>
-                </Col>
-              </FormGroup>
-              <Button type="submit" color="primary">
-                建立
-              </Button>
-            </Form>
-          </Col>
-        </Row>
+        <IssueForm
+          tilte="修改問題"
+          button="修改"
+          handleInputChange={this.handleInputChange}
+          handleSubmit={this.handleSubmit}
+          issue={this.state.issue}
+        />
       </div>
     );
   }
---
+++ frontend/src/public/issue/issue-form.js
@@ -0,0 +1,244 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { Row, Col, Form, FormGroup, Input, Button, Label } from 'reactstrap';
+
+async function fetchTags() {
+  const res = await fetch(API_URL + '/api/issue-option/tag', {
+    method: 'GET',
+    credentials: 'include'
+  });
+  if (res.status === 200) {
+    const tags = await res.json();
+    return tags.map((tag) => {
+      return (
+        <option key={tag.id} value={tag.id}>
+          {tag.name}
+        </option>
+      );
+    });
+  }
+}
+
+async function fetchStatus() {
+  const res = await fetch(API_URL + '/api/issue-option/status', {
+    method: 'GET',
+    credentials: 'include'
+  });
+  if (res.status === 200) {
+    const status = await res.json();
+    return status.map((status) => {
+      return (
+        <option key={status.id} value={status.id}>
+          {status.name}
+        </option>
+      );
+    });
+  }
+}
+
+async function fetchPriorities() {
+  const res = await fetch(API_URL + '/api/issue-option/priority', {
+    method: 'GET',
+    credentials: 'include'
+  });
+  if (res.status === 200) {
+    const priorities = await res.json();
+    return priorities.map((priority) => {
+      return (
+        <option key={priority.id} value={priority.id}>
+          {priority.name}
+        </option>
+      );
+    });
+  }
+}
+
+function getFinishedPercent() {
+  return Array.from(Array(11).keys(), (i) => i * 10).map((num) => {
+    return (
+      <option key={num} value={num}>
+        {num}%
+      </option>
+    );
+  });
+}
+
+class IssueForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: [],
+      status: [],
+      priorities: [],
+      finishedPercent: getFinishedPercent()
+    };
+  }
+
+  componentDidMount() {
+    (async () => {
+      const tags = await fetchTags();
+      const status = await fetchStatus();
+      const priorities = await fetchPriorities();
+      if (Array.isArray(tags)) {
+        this.setState({ tags: tags });
+      }
+      if (Array.isArray(status)) {
+        this.setState({ status: status });
+      }
+      if (Array.isArray(priorities)) {
+        this.setState({ priorities: priorities });
+      }
+    })();
+  }
+
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col sm={{ size: 1, offset: 1 }}>
+            <h2>{this.props.title}</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={{ size: 10, offset: 1 }} className="bg-light">
+            <Form className="mt-3 mb-3" onSubmit={this.props.handleSubmit}>
+              <FormGroup row>
+                <Col sm="1">
+                  <Label for="tagId">追蹤標籤</Label>
+                </Col>
+                <Col sm="11">
+                  <Input
+                    type="select"
+                    name="tagId"
+                    id="tagId"
+                    required
+                    value={this.props.issue.tagId}
+                    onChange={this.props.handleInputChange}
+                  >
+                    {this.state.tags}
+                  </Input>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col sm="1">
+                  <Label for="title">標題</Label>
+                </Col>
+                <Col sm="11">
+                  <Input
+                    type="text"
+                    name="title"
+                    id="title"
+                    maxLength="100"
+                    required
+                    value={this.props.issue.title}
+                    onChange={this.props.handleInputChange}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col sm="1">
+                  <Label for="description">簡述</Label>
+                </Col>
+                <Col sm="11">
+                  <Input
+                    type="textarea"
+                    name="description"
+                    id="description"
+                    value={this.props.issue.description}
+                    onChange={this.props.handleInputChange}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col sm="1">
+                  <Label for="statusId">狀態</Label>
+                </Col>
+                <Col sm="5">
+                  <Input
+                    type="select"
+                    name="statusId"
+                    id="statusId"
+                    value={this.props.issue.statusId}
+                    onChange={this.props.handleInputChange}
+                  >
+                    {this.state.status}
+                  </Input>
+                </Col>
+                <Col sm="1">
+                  <Label for="startDate">開始日期</Label>
+                </Col>
+                <Col sm="5">
+                  <Input type="input" name="startDate" id="startDate" />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col sm="1">
+                  <Label for="statusId">優先權</Label>
+                </Col>
+                <Col sm="5">
+                  <Input
+                    type="select"
+                    name="priorityId"
+                    id="priorityId"
+                    value={this.props.issue.priorityId}
+                    onChange={this.props.handleInputChange}
+                  >
+                    {this.state.priorities}
+                  </Input>
+                </Col>
+                <Col sm="1">
+                  <Label for="endDate">結束日期</Label>
+                </Col>
+                <Col sm="5">
+                  <Input type="input" name="endDate" id="endDate" />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col sm="1">
+                  <Label for="assignedAccount">分派給</Label>
+                </Col>
+                <Col sm="5">
+                  <Input type="input" name="assignedAccount" id="assignedAccount" />
+                </Col>
+                <Col sm="1">
+                  <Label for="estimateWorkHour">預估工時</Label>
+                </Col>
+                <Col sm="5">
+                  <Input
+                    type="input"
+                    name="estimateWorkHour"
+                    id="estimateWorkHour"
+                    value={this.props.issue.estimateWorkHour}
+                    onChange={this.props.handleInputChange}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col sm="1">
+                  <Label for="finishedPercent">完成百分比</Label>
+                </Col>
+                <Col sm="5">
+                  <Input
+                    type="select"
+                    name="finishedPercent"
+                    id="finishedPercent"
+                    value={this.props.issue.finishedPercent}
+                    onChange={this.props.handleInputChange}
+                  >
+                    {this.state.finishedPercent}
+                  </Input>
+                </Col>
+              </FormGroup>
+              <Button type="submit" color="primary">
+                {this.props.button}
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default IssueForm;
