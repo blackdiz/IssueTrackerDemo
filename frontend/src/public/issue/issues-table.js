a254925557fa3985028360a45127175279f7ce01
blackdiz <blackdiz@gmail.com>
2018-07-11 12:23:10 +0800

    問題列表由list改為table呈現

--- frontend/src/public/issue/issues-table.js
+++ frontend/src/public/issue/issues-table.js
@@ -1,18 +1,32 @@
 'use strict';
 
 import React from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { Table } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const IssuesTable = (props) => {
   if (props.issues) {
     const issues = props.issues.map((issue) => {
       return (
-        <ListGroupItem tag="a" href={`${props.match.url}`}>
-          #{issue.id} {issue.name}
-        </ListGroupItem>
+        <tr key={issue.id}>
+          <td>
+            <Link to={`${props.match.url}/${issue.id}`}>#{issue.id}</Link>
+          </td>
+          <td>{issue.title}</td>
+        </tr>
       );
     });
-    return <ListGroup flush>{issues}</ListGroup>;
+    return (
+      <Table responsive striped>
+        <thead>
+          <tr>
+            <th scope="col">編號</th>
+            <th scope="col">標題</th>
+          </tr>
+        </thead>
+        <tbody>{issues}</tbody>
+      </Table>
+    );
   }
 };
 
